1414e6369bec2814b5088e4068ddf7c74d1f9e6a
TribeOfficial <123631444+TribeOfficial@users.noreply.github.com>
2023-06-09 12:51:38 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,8 +6,8 @@ if path.exists("config.env"):
     load_dotenv("config.env")
 
 BOT_TOKEN = environ.get("BOT_TOKEN", None)
-API_ID = int(environ.get("API_ID", 6))
-API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+API_ID = int(environ.get("API_ID", None)
+API_HASH = environ.get("API_HASH", "d18937aebbfef48deb77251aeab6dc43")
 SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
 LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
 GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
